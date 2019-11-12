e12df14582a2ada5155520e78ba6f7c4f873ed22
Anais Ahmed <anaismahmed@uchicago.edu>
2019-11-12 15:46:48 -0600

    routing changes

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Signup from './Signup.js'
 import Login from './Login.js'
 import User from './User.js'
 import Navbar from './Navbar.js'
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/ESLA-frontend/">
+    <Router basename="/ESLA-frontend" >
       <div>
         <Navbar />
         <Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 export default class Home extends Component {
+  constructor(){
+    super();
+    this.state = {
+      ip: ""
+    }
+  }
+
+  componentDidMount(){
+    fetch("https://stark-lake-66426.herokuapp.com/")
+    .then(res => res.json())
+    .then(json => {
+      this.setState({ip: json["but where's here?"]})
+    })
+  }
+
   render(){
     return (
       <>
         <h1>Welcome to ESLA!</h1>
-        <p>ESLA is a database of European sign languages based on grammatical properties.</p>
+        <p>ESLA is a database of European sign languages based on grammatical properties. Your IP address is {this.state.ip}.</p>
       </>
     )
   }
