4cfc07a1885b18c009aa617478c72150097d61be
Anais Ahmed <anaismahmed@uchicago.edu>
2019-11-12 16:31:59 -0600

    heroku issues

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/ESLA-frontend" >
+    <Router >
       <div>
         <Navbar />
         <Switch>
